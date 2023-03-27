adc24f0134d001fab0193ee01cb3ddc2c53b4dab
Soungjae <soungj94@gmail.com>
2023-03-26 22:07:50 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+// superbowlWin(record)
+// 1) returns a year the Denver Broncos won the superbowl
+// 2) returns undefined if the record has no win objects
+
+// function superbowlWin(record) {
+//   const winningYear = record.find(function (entry) {
+//     return entry.result === "W";
+//   });
+//   return winningYear ? winningYear.year : undefined;
+// }
+
+function superbowlWin(record) {
+  const winningYear = record.find((entry) => entry.result === "W");
+  return winningYear ? winningYear.year : undefined;
+}
